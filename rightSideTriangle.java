cef6910c9a5042a3c2bbb0e534905efd2d1517cd
MAITITE MOHAMED <maititemohamed@gmail.com>
2022-10-02 12:52:07 +0100

    rightSideTriangle

--- rightSideTriangle.java
+++ rightSideTriangle.java
@@ -1,18 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        for(int j=1;j<=5;j++){
-            
-         for(int i=1;i<=5-j;i++){
-          System.out.print(" ");
-          }
-           for(int i=1;i<=j;i++){
-           System.out.print("*");
+        for(int j=0;j<5;j++){
+           for(int i=0;i<j;i++){
+           System.out.print("  ");
            }
-          
-                  System.out.println();
-
-        }
-       
-        
+            for(int i=0;i<5-j;i++){
+           System.out.print("* ");
+           }
+             System.out.println();
+        }   
     }
 }
