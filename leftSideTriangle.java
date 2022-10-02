2c2720e0ebc4f2aeb4fc44109141223055e3b29c
MAITITE MOHAMED <maititemohamed@gmail.com>
2022-10-02 12:33:07 +0100

    ## right side  triangle

---
+++ leftSideTriangle.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int j=0;j<5;j++){
+            
+         for(int i=0;i<5-j;i++){
+          System.out.print(" *");
+          }
+         
+          
+                  System.out.println();
+
+        }
+       
+        
+    }
+}
