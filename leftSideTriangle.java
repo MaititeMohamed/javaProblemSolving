9e1e2d81350215e89c2ed94effdf4c036aec6053
MAITITE MOHAMED <maititemohamed@gmail.com>
2022-10-02 12:37:49 +0100

    left side Triangle

--- leftSideTriangle.java
+++ leftSideTriangle.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        for(int j=0;j<5;j++){
+        for(int j=1;j<=5;j++){
             
-         for(int i=0;i<5-j;i++){
-          System.out.print(" *");
+         for(int i=1;i<=5-j;i++){
+          System.out.print("  ");
           }
-         
+           for(int i=1;i<=j;i++){
+           System.out.print("* ");
+           }
           
                   System.out.println();
 
